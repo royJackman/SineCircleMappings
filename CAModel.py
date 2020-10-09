f52cefa9a0b16e457802232583c5fa4366ef5391
Roy <jackman.roy@gmail.com>
2020-10-09 00:55:15 -0400

    Added Cellular Automata model

---
+++ CAModel.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+import numpy as np
+
+tf.config.run_functions_eagerly(True)
+
+class CAModel(tf.keras.Model):
+    def __init__(self, channels=18):
+        super().__init__()
+        self.channels = channels
+
+        self.dmodel = tf.keras.Sequential([
+            tf.keras.layers.Conv1D(128, 1, activation=tf.nn.relu),
+            tf.keras.layers.Conv1D(self.channels, 1, activation=None, kernel_initializer=tf.zeros_initializer)
+        ])
+
+        self(tf.zeros([1, 1, channels, channels]))
+
+    @tf.function
+    def percieve(self, x):
+        identity = np.zeros(self.channels).reshape((1, -1)).astype('float32')
+        identity[-1] = 1.0
+        uniform = (1.0/self.channels) * np.ones(self.channels).reshape((1, -1)).astype('float32')
+        linear = np.linspace(0.0, 1.0, self.channels + 1)[1:].reshape((1, -1)).astype('float32')
+        log = np.logspace(0.0, 1.0, self.channels + 1)[1:].reshape((1, -1)).astype('float32')
+        kernel = tf.stack([identity, uniform, linear, log], -1)[:, :, None, :]
+        kernel = tf.repeat(kernel, self.channels, 2)
+        y = tf.nn.depthwise_conv2d(x, kernel, [1, 1, 1, 1], 'SAME')
+        return y
+
+    @tf.function
+    def call(self, x, step_size=1.0):
+        y = self.percieve(x)
+        dx = self.dmodel(y) * step_size
+        x += dx
+        return x
+
+CAModel().dmodel.summary()
