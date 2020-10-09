f0426736c0074569fcdf6f316018ed2e8563440f
Roy <jackman.roy@gmail.com>
2020-10-09 02:23:54 -0400

    Added SCM functionality to CAModel

--- CAModel.py
+++ CAModel.py
@@ -4,24 +4,25 @@ import numpy as np
 tf.config.run_functions_eagerly(True)
 
 class CAModel(tf.keras.Model):
-    def __init__(self, channels=18):
+    def __init__(self, channels=19, past_notes=16):
         super().__init__()
         self.channels = channels
+        self.past_notes = past_notes
 
         self.dmodel = tf.keras.Sequential([
-            tf.keras.layers.Conv1D(128, 1, activation=tf.nn.relu),
-            tf.keras.layers.Conv1D(self.channels, 1, activation=None, kernel_initializer=tf.zeros_initializer)
+            tf.keras.layers.Conv2D(128, 1, activation=tf.nn.relu),
+            tf.keras.layers.Conv2D(self.channels, 1, activation=None, kernel_initializer=tf.zeros_initializer)
         ])
 
-        self(tf.zeros([1, 1, channels, channels]))
+        self(tf.zeros([1, 1, self.past_notes, channels]))
 
     @tf.function
     def percieve(self, x):
-        identity = np.zeros(self.channels).reshape((1, -1)).astype('float32')
+        identity = np.zeros(self.past_notes).reshape((1, -1)).astype('float32')
         identity[-1] = 1.0
-        uniform = (1.0/self.channels) * np.ones(self.channels).reshape((1, -1)).astype('float32')
-        linear = np.linspace(0.0, 1.0, self.channels + 1)[1:].reshape((1, -1)).astype('float32')
-        log = np.logspace(0.0, 1.0, self.channels + 1)[1:].reshape((1, -1)).astype('float32')
+        uniform = (1.0/self.past_notes) * np.ones(self.past_notes).reshape((1, -1)).astype('float32')
+        linear = np.linspace(0.0, 1.0, self.past_notes + 1)[1:].reshape((1, -1)).astype('float32')
+        log = np.logspace(0.0, 1.0, self.past_notes + 1)[1:].reshape((1, -1)).astype('float32')
         kernel = tf.stack([identity, uniform, linear, log], -1)[:, :, None, :]
         kernel = tf.repeat(kernel, self.channels, 2)
         y = tf.nn.depthwise_conv2d(x, kernel, [1, 1, 1, 1], 'SAME')
@@ -31,7 +32,11 @@ class CAModel(tf.keras.Model):
     def call(self, x, step_size=1.0):
         y = self.percieve(x)
         dx = self.dmodel(y) * step_size
+        curr_theta =  x.numpy()[:, :, -1, :]
         x += dx
+        npx = x.numpy()
+        npx[:, :, -1, :] = curr_theta + npx[:, :, -3, :] + npx[:, :, -2, :] * ((-1/2)*np.sin(curr_theta * (2*np.pi)))
+        x = tf.convert_to_tensor(npx)
         return x
 
 CAModel().dmodel.summary()
