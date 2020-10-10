c583932d38fa2e335eba3eaef41792ac41fdd377
Roy <jackman.roy@gmail.com>
2020-10-10 14:21:24 -0400

    Load and clean data

--- DataLoader.py
+++ DataLoader.py
@@ -2,3 +2,27 @@ import numpy as np
 
 with open('bach_chorales.npy', 'rb') as f:
     bach_chorales = np.load(f, allow_pickle=True)
+
+def listify(chorale):
+    retval = []
+    for note in chorale:
+        while len(retval) < note[0]:
+            retval.append(0)
+        for i in range(note[2]):
+            retval.append(note[1])
+    return retval
+
+list_chorales = np.array([listify(chorale) for chorale in bach_chorales])
+
+def stagger(list_chorale, window=16):
+    buffer = [0] * window
+    X = []
+    Y = []
+    for note in list_chorale:
+        X.append(buffer)
+        Y.append(note)
+        buffer = buffer[1:]
+        buffer.append(note)
+    X.append(buffer)
+    Y.append(0)
+    return np.array(X), np.array(Y)
