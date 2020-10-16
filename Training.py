af2ad836c57482e091d5ad7ed8970d597acbad4a
Roy <jackman.roy@gmail.com>
2020-10-15 22:24:54 -0400

    Basic graphing added to learning loop

--- Training.py
+++ Training.py
@@ -6,9 +6,9 @@ from DataLoader import list_chorales, stagger
 import matplotlib.pyplot as plt
 
 target = tf.pad(np.array(list_chorales[0]).astype('float32').reshape((1, -1)), [(0, 0), (15, 0)])
-seed = np.zeros([1,target.shape[1],19], np.float32)
+seed = np.zeros([1,target.shape[1],17], np.float32)
 first = np.nonzero(list_chorales[0])[0]
-seed[0, first[0]] = list_chorales[0][first[0]]
+seed[0, first[0], -1 ] = list_chorales[0][first[0]]
 
 def loss_f(x):
     return tf.reduce_mean(tf.square(x[..., -1] - target))
@@ -35,10 +35,22 @@ def train_step(x):
     trainer.apply_gradients(zip(grads, ca.weights))
     return x, loss
 
+notes = range(len(list_chorales[0]))
+plt.ion()
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+ax1.plot(notes, list_chorales[0])
+pred, = ax2.plot(notes, [0] * len(list_chorales[0]))
+plt.show()
+
 for i in range(8000+1):
     x0 = np.repeat(seed[None, ...], 8, 0)
     x, loss = train_step(x0)
+    
+    ax2.plot(notes, x.numpy()[0, :, :, -1].flatten().tolist()[15:])
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+
     step_i = len(loss_log)
     loss_log.append(loss.numpy())
 
-    print('\r step: %d, log10(loss): %.3f'%(len(loss_log), np.log10(loss)), end='')
+    print('\r step: %d, log10(loss): %.3f'%(i+1, np.log10(loss)), end='')
