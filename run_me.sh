26cf3e70ff66da1b75b99310c379dcdd64d51d73
Roy <jackman.roy@gmail.com>
2020-11-12 18:09:40 -0500

    Folder fix

--- run_me.sh
+++ run_me.sh
@@ -9,7 +9,7 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 
 conda create -y --name py38 python=3.8
-conda install --force-reinstall -y -q --name py38 -c conda-forge --file requirements.txt
+conda install --force-reinstall -y -q --name py38 -c conda-forge --file ~/CAMusic/requirements.txt
 conda activate py38
 
 python3 ~/CAMusic/Training.py -s -m ~/CAMusic/midis/linkin_park-one_step_closer.mid -e 100000 -w 12
