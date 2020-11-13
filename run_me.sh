bdb85412232f3fdd8a2a720a3e9bbee53dde7f96
Roy <jackman.roy@gmail.com>
2020-11-12 22:24:41 -0500

    Conda remaking fix

--- run_me.sh
+++ run_me.sh
@@ -8,9 +8,7 @@
 
 source ~/miniconda3/etc/profile.d/conda.sh
 
-conda create -y --name py38 python=3.8
 conda activate py38
-conda install --file ~/CAMusic/requirements.txt
 
 python3 ~/CAMusic/Training.py -s -m ~/CAMusic/midis/linkin_park-one_step_closer.mid -e 100000 -w 12
 
