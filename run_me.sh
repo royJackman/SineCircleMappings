2d658dcb53a6d8f7680228ff31cca9dba643f8ae
Roy <jackman.roy@gmail.com>
2020-11-22 19:40:59 -0500

    Added mailing when complete

--- run_me.sh
+++ run_me.sh
@@ -6,6 +6,8 @@
 #SBATCH -e ./log/res_%j.err
 #SBATCH --partition=2080ti-long
 #SBATCH --cpus-per-task=32
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=royjackman@umass.edu	
 
 # --gres=gpu:4
 # -p gpu
@@ -16,7 +18,7 @@ source ~/miniconda3/etc/profile.d/conda.sh
 
 conda activate py38
 
-python3 ~/CAMusic/Training.py -b 1 -e 50 -i 24 -m ~/CAMusic/midis/linkin_park-one_step_closer.mid -n LP-OSC-$now-slurm -o ~/CAMusic/outputs -r 2 -s -w 12
+python3 ~/CAMusic/Training.py -b 2 -e 10000 -i 24 -m ~/CAMusic/midis/linkin_park-one_step_closer.mid -n LP-OSC-$now-slurm -o ~/CAMusic/outputs -r 2 -s -w 12
 
 conda deactivate
 
