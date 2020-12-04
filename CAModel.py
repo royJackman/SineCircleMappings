49234a7a19a92d60607807570890ee061b84012a
Roy <jackman.roy@gmail.com>
2020-12-04 09:51:07 -0500

    More piano roll additions

--- CAModel.py
+++ CAModel.py
@@ -4,18 +4,20 @@ import numpy as np
 tf.config.run_functions_eagerly(True)
 
 class CAModel(tf.keras.Model):
-    def __init__(self, past_notes=16, width=1, filters=128):
+    def __init__(self, past_notes=16, width=1, filters=128, piano_roll=False):
         super().__init__()
         self.past_notes = past_notes
         self.width = width
         self.filters = filters
+        self.piano_roll = piano_roll
 
-        self.dmodel = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(self.filters, 1, activation=tf.nn.relu),
-            tf.keras.layers.Conv2D(self.past_notes + 1, 1, activation=None, kernel_initializer=tf.keras.initializers.zeros())
-        ])
+        layers = [tf.keras.layers.MaxPool2D(pool_size=(3,3), padding='same', strides=(1,1))] if self.piano_roll else []
+        layers.append(tf.keras.layers.Conv2D(self.filters, 1, activation=tf.nn.relu))
+        layers.append(tf.keras.layers.Conv2D(self.past_notes + 1, 1, activation=None, kernel_initializer=tf.keras.initializers.zeros()))
 
-        self(tf.zeros([1, self.width, self.past_notes, self.past_notes + 1]))
+        self.dmodel = tf.keras.Sequential(layers)
+
+        self(tf.zeros([1, 128 if self.piano_roll else self.width, self.past_notes, self.past_notes + 1]))
 
     @tf.function
     def perceive(self, x):
