9c79fd50dc4442f446a9052c64f9803bd625a748
Roy <jackman.roy@gmail.com>
2020-12-10 16:01:56 -0500

    Single example RNN overdampened

--- SCMNet.py
+++ SCMNet.py
@@ -2,25 +2,56 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+device = torch.device('cpu')
+
 def scm(theta, alpha=1.0, k=1.0, omega=0.16): return alpha * theta + omega + (k/(2 * np.pi)) * np.sin(2 * np.pi * theta)
 
 class SCMNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.rnn = nn.RNN(1, 20, 2)
-        self.linear = nn.Linear(20, 3)
+    def __init__(self, input_size, output_size, hidden_dim, n_layers):
+        super(SCMNet, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        
+        self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_dim, output_size * 3)
         self.params = []
     
-    def forward(self, x, hidden=torch.zeros(2, 1, 20).to(torch.device('cpu'))):
+    def forward(self, x):
+        batch_size = x.size(0)
+        hidden = self.init_hidden(batch_size)
         theta_n = x
-        x, hidden = self.rnn(x)
-        x = x.view(-1, 20)
+        x, hidden = self.rnn(x, hidden)
+        x = x.contiguous().view(-1, self.hidden_dim)
         x = self.linear(x)
+        self.params = x
         return scm(theta_n, x[0][0], x[0][1], x[0][2]), hidden
+        # return x, hidden
+    
+    def init_hidden(self, batch_size):
+        hidden = torch.zeros(self.n_layers, batch_size, self.hidden_dim).to(device)
+        return hidden
 
 torch.manual_seed(0)
-model = SCMNet()
-inp = np.array([[[0]]])
+model = SCMNet(1, 1, 12, 1)
+model = model.to(device)
+inp = np.array([[[0], [1], [2], [3], [4], [5], [6], [7]]])
+# outp = np.array([[[1], [2], [3], [4], [5], [6], [7], [8]]])
+outp = np.array([[8]])
 x = torch.tensor(inp, dtype=torch.float)
-out, hidden = model(x)
-print(out)
+y = torch.tensor(outp, dtype=torch.float)
+epochs = 270
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+x = x.to(device)
+for e in range(1, epochs + 1):
+    optimizer.zero_grad()
+    output, hidden = model(x)
+    output = output.to(device)
+    y = y.to(device)
+    loss = criterion(output, y)
+    loss.backward()
+    optimizer.step()
+
+    if e%15 == 0:
+        print('Epoch: {}/{}.............'.format(e, epochs), end=' ')
+        print("Loss: {:.6f}".format(loss.item()))
