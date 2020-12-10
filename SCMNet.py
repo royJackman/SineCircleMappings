6214b6280694df9c4bfd45ba73a53d7691b12673
Roy <jackman.roy@gmail.com>
2020-12-09 19:13:36 -0500

    Basic RNN

--- SCMNet.py
+++ SCMNet.py
@@ -7,15 +7,20 @@ def scm(theta, alpha=1.0, k=1.0, omega=0.16): return alpha * theta + omega + (k/
 class SCMNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv = nn.Conv2d(1, 128, 3)
-        self.linear = nn.Linear(256, 2)
+        self.rnn = nn.RNN(1, 20, 2)
+        self.linear = nn.Linear(20, 3)
+        self.params = []
     
-    def forward(self, x):
-        x = self.conv(x)
-        x = x.view(-1, 256)
-        return self.linear(x)
+    def forward(self, x, hidden=torch.zeros(2, 1, 20).to(torch.device('cpu'))):
+        theta_n = x
+        x, hidden = self.rnn(x)
+        x = x.view(-1, 20)
+        x = self.linear(x)
+        return scm(theta_n, x[0][0], x[0][1], x[0][2]), hidden
 
 torch.manual_seed(0)
 model = SCMNet()
-inp = np.array([[[[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]]])
+inp = np.array([[[0]]])
 x = torch.tensor(inp, dtype=torch.float)
+out, hidden = model(x)
+print(out)
