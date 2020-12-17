e1628bdb506334fb5007d7b337c91fc167b99ca2
Roy Jackman <jackman.roy@gmail.com>
2020-12-17 16:20:00 -0500

    A little bit off the top

--- SCMNet.py
+++ SCMNet.py
@@ -17,17 +17,6 @@ device = torch.device('cpu')
 
 def scm(theta, alpha=1.0, k=1.0, omega=0.16): return alpha * theta + omega + (k/(2 * np.pi)) * np.sin(2 * np.pi * theta)
 
-def generate_data(start, end, points):
-    full = np.linspace(start, end, points+1)
-    if args.func_string is None:
-        if args.func == 'sine':
-            full = np.sin(full)
-    else:
-        exp = parse_expr(args.func_string, transformations=(standard_transformations + (implicit_multiplication_application,)))
-        test = [exp.evalf(subs={'x': i}) for i in full]
-        full = np.asarray([exp.evalf(subs={'x': i}) for i in full]).astype('float')
-    return full[:-1], full[1:]
-
 class SCMNet(nn.Module):
     def __init__(self, input_size, output_size, hidden_dim, n_layers):
         super(SCMNet, self).__init__()
@@ -53,41 +42,3 @@ class SCMNet(nn.Module):
         return hidden
 
 torch.manual_seed(0)
-model = SCMNet(1, 1, args.hidden_dim, 1)
-model = model.to(device)
-
-hidden = None
-epochs = 100
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-
-plt.figure(1)
-plt.get_current_fig_manager().window.state('zoomed')
-plt.ion()
-
-data = generate_data(args.range[0], args.range[1], 1000)
-
-for step in range(epochs):
-    start, end = step * 10, (step+1)*10
-    x_np = data[0][start:end]
-    y_np = data[1][start:end]
-    x = torch.from_numpy(x_np[np.newaxis, :, np.newaxis]).float()
-    y = torch.from_numpy(y_np[np.newaxis, :, np.newaxis])
-
-    prediction, hidden = model(x, hidden)
-    hidden = hidden.data
-
-    loss = criterion(prediction.double(), y)
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-    steps = [*range(start, end)]
-
-    plt.plot(steps, y_np.flatten(), 'r-')
-    plt.plot(steps, np.mean(prediction.data.numpy()[0, :, :, 0], axis=0), 'b-')
-    plt.draw(); plt.pause(0.05)
-
-plt.ioff()
-plt.show()
-print(model.params.detach().numpy())
