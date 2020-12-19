417330434d7bf0c2de78ceb19ea3eaed35b66c5d
Roy Jackman <jackman.roy@gmail.com>
2020-12-18 23:29:13 -0500

    Basic SCM net with backprop

--- SCMNet.py
+++ SCMNet.py
@@ -1,6 +1,8 @@
 import argparse
+import random
 import torch
 import torch.nn as nn
+import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,30 +17,52 @@ args = parser.parse_args()
 
 device = torch.device('cpu')
 
+torch.pi = torch.acos(torch.zeros(1)).item() * 2
+
 def scm(theta, alpha=1.0, k=1.0, omega=0.16): return alpha * theta + omega + (k/(2 * np.pi)) * np.sin(2 * np.pi * theta)
 
-class SCMNet(nn.Module):
-    def __init__(self, input_size, output_size, hidden_dim, n_layers):
-        super(SCMNet, self).__init__()
-        self.hidden_dim = hidden_dim
-        self.n_layers = n_layers
-        
-        self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True)
-        self.linear = nn.Linear(hidden_dim, output_size * 3)
-        self.params = []
-    
-    def forward(self, x, hidden):
-        theta_n = x
-        x, hidden = self.rnn(x, hidden)
-        
-        outs = []
-        for time_step in range(x.size(1)):
-            self.params = self.linear(x[:, time_step, :]).flatten()
-            outs.append(scm(theta_n, self.params[0], self.params[1], self.params[2]))
-        return torch.stack(outs, dim=1), hidden
+def tensor_scm(thetas, alphas, ks, omegas): return torch.mul(alphas, thetas) + omegas + torch.mul(torch.mul(ks, (1.0/(2.0 * torch.pi))), torch.sin(torch.mul(2.0 * torch.pi, thetas)))
 
-    def init_hidden(self, batch_size):
-        hidden = torch.zeros(self.n_layers, batch_size, self.hidden_dim).to(device)
-        return hidden
+class SCMNet(nn.Module):
+    def __init__(self, input_size, output_size, reservoir_size, input_spread=1, output_spread=1):
+        super(SCMNet, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.input_spread = input_spread
+        self.output_spread = output_spread
+
+        self.inputs = random.sample(range(reservoir_size), self.input_spread)
+        self.outputs = random.sample(range(reservoir_size), self.output_spread)
+
+        self.driver = torch.zeros(reservoir_size)
+        self.driver[self.inputs] = 1.0
+
+        self.alphas = nn.Parameter(torch.rand(reservoir_size))
+        self.ks = nn.Parameter(torch.rand(reservoir_size))
+        self.omegas = nn.Parameter(torch.rand(reservoir_size))
+        self.reservoir = torch.from_numpy(nx.to_numpy_array(nx.generators.random_graphs.watts_strogatz_graph(reservoir_size, 4, 0.5))).float()
+    
+    def forward(self, x, reservoir_state):
+        driven_state = reservoir_state + x * self.driver
+        thetas = torch.matmul(driven_state, self.reservoir)
+        thetas = tensor_scm(thetas, self.alphas, self.ks, self.omegas)
+        return torch.sum(thetas[self.outputs]), thetas
 
 torch.manual_seed(0)
+model = SCMNet(1, 1, 10, 2, 4)
+reservoir0 = torch.rand(10)
+crit = nn.MSELoss()
+opti = torch.optim.Adam(model.parameters(), lr=0.01)
+
+for epoch in range(1):
+    running_loss =0.0
+    for i in range(100):
+        x = i/100.0
+        y = (i + 1)/100.0
+        opti.zero_grad()
+        out, reservoir0 = model(x, reservoir0)
+        loss = crit(out, torch.tensor(y))
+        opti.step()
+        print(loss.item())
+
+print('Done')
