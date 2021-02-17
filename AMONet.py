36b8d269aa43264ae02d63a942332b2f6bdf922e
Roy <jackman.roy@gmail.com>
2021-02-16 20:40:25 -0500

    Adding NN model

--- AMONet.py
+++ AMONet.py
@@ -10,6 +10,7 @@ import HarmonicNN as hnn
 from tqdm import trange
 from statistics import mean
 from TestRNN import testRNN
+from TestNN import testNN
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -20,7 +21,7 @@ parser = argparse.ArgumentParser('Compare Harmonic NN to RNN of equal volume')
 parser.add_argument('-n', '--nodes', type=int, dest='nodes', default=3, help='Number of internal nodes')
 args = parser.parse_args()
 
-torch.manual_seed(1)
+torch.manual_seed(2)
 # model = torch.jit.script(msn.MultiSCMNet(1, 1, [8, 8, 8]))
 model = torch.jit.script(hnn.MultilayerHarmonicNN(1, 1, None))
 print('HNN params:', sum([len(p.flatten()) for p in model.parameters()]))
@@ -34,8 +35,14 @@ print('RNN params:', sum([len(p.flatten()) for p in RNN.parameters()]))
 RNN_crit = torch.nn.MSELoss()
 RNN_opti = torch.optim.Adam(RNN.parameters(), lr = 0.01)
 
+NN = testNN(1, 1, args.nodes)
+print('NN params:', sum([len(p.flatten()) for p in NN.parameters()]))
+NN_crit = torch.nn.MSELoss()
+NN_opti = torch.optim.Adam(NN.parameters(), lr = 0.01)
+
 roll = []
 RNN_roll = []
+NN_roll = []
 actual_roll = []
 
 fig, axs = plt.subplots(1, 1)
@@ -45,6 +52,7 @@ torch.autograd.set_detect_anomaly(True)
 
 total_loss = 0.0
 RNN_total = 0.0
+NN_total = 0.0
 for repetition in range(1):
     plt.cla()
     x = data[0].reshape(1,1).float()
@@ -53,40 +61,54 @@ for repetition in range(1):
         pred = model(x)
         RNN_pred, hidden = RNN(x[np.newaxis, :].float(), hidden.float())
         hidden = hidden.data
+        NN_pred = NN(x.float())
 
         roll.append(pred.item())
         RNN_roll.append(RNN_pred.item())
+        NN_roll.append(NN_pred.item())
         actual_roll.append(y.item())
 
         loss = crit(pred.double(), y)
         RNN_loss = RNN_crit(RNN_pred.double(), y.reshape([]))
+        NN_loss = NN_crit(NN_pred.double(), y)
 
-        total_loss += loss.item()
-        RNN_total += RNN_loss.item()
+        if int(step) > 100:
+            total_loss += loss.item()
+            RNN_total += RNN_loss.item()
+            NN_total += NN_loss.item()
 
         opti.zero_grad()
         RNN_opti.zero_grad()
+        NN_opti.zero_grad()
 
         loss.backward(retain_graph=True)
         RNN_loss.backward(retain_graph=True)
+        NN_loss.backward(retain_graph=True)
 
         opti.step()
         RNN_opti.step()
+        NN_opti.step()
 
         plotpred = pred.detach().numpy()
         plotRNN = RNN_pred.detach().numpy()
+        plotNN = NN_pred.detach().numpy()
+        
+        axs.set_title('Atlantic Multi-Decadal Oscillation')
 
         axs.plot(int(step), plotpred[0], 'rx', label='Prediction')
         axs.plot(int(step), plotRNN, 'bx', label='RNN Benchmark')
+        axs.plot(int(step), plotNN, 'kx', label='NN Benchmark')
         axs.plot(int(step), y[0], 'gx', label='Actual')
 
         if len(roll) > 12:
             roll = roll[1:]
             RNN_roll = RNN_roll[1:]
+            NN_roll = NN_roll[1:]
             actual_roll = actual_roll[1:]
 
         axs.plot(int(step), mean(roll), 'ro', label='Pred Rolling Avg')
         axs.plot(int(step), mean(RNN_roll), 'bo', label='RNN Rolling Avg')
+        axs.plot(int(step), mean(NN_roll), 'ko', label='NN Rolling Avg')
         axs.plot(int(step), mean(actual_roll), 'go', label='Actual Rolling Avg')
 
         axs.set_xlim(max(-1, int(step) - 100), int(step))
@@ -101,6 +123,7 @@ for repetition in range(1):
         if (int(step) - 1) % 100 == 0:
             print('Average error HNN:', total_loss/int(step))
             print('Average error RNN:', RNN_total/int(step))
+            print('Average error NN:', NN_total/int(step))
         
         plt.draw(); plt.pause(0.001)
 
