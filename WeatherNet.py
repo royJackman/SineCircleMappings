fdc7b31aa88c3cbd754601b5365f74fb95a00b54
Roy <jackman.roy@gmail.com>
2021-02-24 15:39:10 -0500

    Longitudinal weathernet update

--- WeatherNet.py
+++ WeatherNet.py
@@ -13,7 +13,11 @@ months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 
 data = pd.read_csv('Data/amo_monthly.csv')
 data = torch.tensor(data[months].values)
 
-train_count = 140
+# plt.plot(data[:, 0])
+# plt.show()
+# sys.exit()
+
+train_count = 139
 
 torch.manual_seed(0)
 torch.autograd.set_detect_anomaly(True)
@@ -21,7 +25,7 @@ models = {}
 crits = {}
 optis = {}
 for month in months:
-    models[month] = torch.jit.script(MultilayerHarmonicNN(1, 1))
+    models[month] = torch.jit.script(MultilayerHarmonicNN(1, 1, [6,6]))
     crits[month] = torch.nn.MSELoss()
     optis[month] = torch.optim.Adam(models[month].parameters(), lr=0.01)
 # model = torch.jit.script(MultilayerHarmonicNN(1, 12, [4, 4]))
@@ -35,12 +39,16 @@ plt.figure(1)
 plt.get_current_fig_manager().window.state('zoomed')
 plt.ion()
 
+fig, axs = plt.subplots(3, 4)
+
 for i in range(100):
     round_loss = 0.0
+    predictions = np.zeros(data.shape)
     for step in range(train_count):
         for j, month in enumerate(months):
             pred = models[month](torch.tensor(step).reshape((1,1)))
-            loss = crits[month](pred, data[step,j].reshape((1,1)))
+            predictions[step, j] = pred.item()
+            loss = crits[month](pred.reshape((1,1)), data[step,j].reshape((1,1)))
             round_loss += loss.item()/12.0
             optis[month].zero_grad()
             loss.backward(retain_graph=True)
@@ -54,14 +62,33 @@ for i in range(100):
     test_loss = 0.0
     for step in range(data.shape[0]-train_count):
         example_loss = 0.0
+        prediction = []
         for j, month in enumerate(months):
             pred = models[month](torch.tensor(step + train_count).reshape((1,1)))
-            example_loss += abs(crits[month](pred, data[step + train_count, j]).item())/12.0
+            predictions[step + train_count, j] = pred.item()
+            example_loss += abs(crits[month](pred.reshape((1,1)), data[step + train_count, j].reshape((1,1))).item())/12.0
+            prediction.append(pred.item())
+        # if step < 12:
+        #     c = int(step % 4)
+        #     r = int((step - c)/4)
+        #     axs[r][c].cla()
+        #     axs[r][c].plot(prediction)
+        #     axs[r][c].plot(data[step+train_count])
         # pred = model(torch.tensor(step + train_count).reshape((1,1)))
         # test_loss += abs(crit(pred, data[step + train_count]).item())
         test_loss += example_loss
     if (i+1)%10 == 0:
         print(f'Round {i} avg training loss {round_loss/float(train_count)}')
         print(f'Rount {i} avg test loss {test_loss/float(data.shape[0]-train_count)}\n------------')
+        
+    for j in range(3):
+        for k in range(4):
+            axs[j][k].cla()
+            axs[j][k].plot(data[:, 4*j + k])
+            axs[j][k].plot(predictions[:, 4*j + k])
+
+    plt.gcf().canvas.draw()
+    plt.gcf().canvas.flush_events()
+    # plt.draw(); plt.pause(0.02)
 
 print(f'{models["Jan"](torch.tensor(150).reshape((1,1)))}\n{data[150,0]}')
