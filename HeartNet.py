301c5685d2f9f155119202eebde51623cd009b4a
Roy <jackman.roy@gmail.com>
2021-03-07 03:55:30 -0500

    HeartNet cleanup

--- HeartNet.py
+++ HeartNet.py
@@ -18,7 +18,7 @@ print(target.shape)
 data = data[:, :-1]
 
 examples = data.shape[0]
-epochs = 100
+epochs = 300
 eighty = int(0.8 * examples)
 twenty = examples - eighty
 
@@ -38,13 +38,14 @@ for i, a in enumerate(architectures):
     res = a.pop()
     print(f'Model {i + 1}: {len(res)} layer(s) with size(s) {res}')
     for j, l in enumerate(a):
-        print(f'Layer {j+1}: {l[0]} sin, {l[1]} tanh, {l[2]} relu')
+        print(f'Layer {j+1}: {l[0]} sin, {l[1]} tanh, {l[2]} log')
     model = MixedNet(13, 1, res, a).double()
     crit = torch.nn.MSELoss()
     opti = torch.optim.Adam(model.parameters(), lr = 0.01)
 
     arch_loss = 0.0
     min_arch = 1000.0
+    arch_test = 0.0
     for e in range(epochs):
         i = 0
         batch = 0
@@ -58,9 +59,13 @@ for i, a in enumerate(architectures):
             pred = model(data[last:i, :].double())
             loss = crit(pred, target[last:i].double())
             epoch_loss += loss.item()
-            if loss.item() < min_arch:
-                min_arch = loss.item()
             loss.backward(retain_graph=True)
             opti.step()
         arch_loss += epoch_loss/float(epochs)
-    print(f'Avg MSE: {arch_loss}, Best MSE: {min_arch}\n----------------------')
+        
+        pred = model(data[eighty:].double())
+        loss = crit(pred, target[eighty:].double())
+        if loss.item() < min_arch:
+            min_arch = loss.item()
+        arch_test += loss.item()/float(epochs)
+    print(f'Avg train MSE: {arch_loss}, Avg test MSE: {arch_test}, Best test MSE: {min_arch}\n----------------------')
