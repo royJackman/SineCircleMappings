27d36cff24cb3794ac7b433c9cddcf0a319d697b
Roy Jackman <royJackman@users.noreply.github.com>
2021-04-21 21:11:39 -0400

    AMOBatch v fft

--- AMOBatch.py
+++ AMOBatch.py
@@ -11,11 +11,23 @@ from tqdm import trange
 from statistics import mean
 from TestRNN import testRNN
 
+nodes = [64, 32, 16]
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
 data = pd.read_csv('Data/amo_monthly.csv', delim_whitespace=True)
 data = torch.tensor(data[['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']].values).flatten()
-data = data[:, np.newaxis]
+sp = np.fft.fft(data.clone().detach().cpu().numpy())
+data = data[:, np.newaxis].to(device)
+t = np.arange(len(data))
+freq = np.fft.fftfreq(t.shape[-1])
+
+middle = len(sp) // 2
+empty = np.zeros(sp.shape)
+node_half = sum(nodes) // 2
+empty[:node_half] = sp[:node_half]
+empty[-node_half:] = sp[-node_half:]
+ifft = np.fft.ifft(empty)
+fft_score = sum((data.detach().cpu().numpy().flatten() - ifft.real) ** 2)
+print('FFT:', fft_score)
 
 parser = argparse.ArgumentParser('Learn AMO data with with HNN')
 parser.add_argument('-e', '--epochs', type=int, dest='epochs', default=2000, help='Number of epochs')
@@ -23,36 +35,39 @@ parser.add_argument('-n', '--nodes', type=int, dest='nodes', default=3, help='Nu
 args = parser.parse_args()
 
 torch.manual_seed(2)
-model = hnn.MultilayerHarmonicNN(1,1,[32, 24, 16, 8])
+model = hnn.MultilayerHarmonicNN(1,1,nodes).to(device)
+# model = msn.MultiSCMNet(1, 1, nodes).to(device).double()
 print(f'HNN Params: {sum([len(p.flatten()) for p in model.parameters()])}')
 crit = torch.nn.MSELoss()
-opti = torch.optim.Adam(model.parameters(), lr=0.01)
+opti = torch.optim.Adam(model.parameters(), lr=1e-4)
 
 torch.autograd.set_detect_anomaly(True)
 losses = []
-inp = torch.arange(1, 1981)[:, np.newaxis]
-guess = torch.mul(torch.mean(data), torch.ones((1980, 1)))
+inp = torch.arange(1, 1981)[:, np.newaxis].to(device)
+guess = torch.mul(torch.mean(data), torch.ones((1980, 1)).to(device))
 hiscore = crit(data, guess).item()
 print(f'Guess to beat: {hiscore}')
 broken = False
 
 for r in trange(args.epochs):
     opti.zero_grad()
-    pred = model(inp)
+    pred = model(inp.double())
     loss = crit(pred, data)
     losses.append(loss.item())
-    if not broken and loss.item() < hiscore:
-        print(f'Record broken on epoch {int(r) + 1}')
+    if not broken and loss.item() < fft_score:
+        print(f'FFT score broken on epoch {int(r) + 1} with loss {loss.item()}')
         broken = True
     loss.backward(retain_graph=True)
     opti.step()
 
 print(np.mean(np.array(losses[-50:])))
 
-plt.plot(losses[-1000])
+plt.plot(losses)
+plt.yscale("log")
 plt.figure()
 
-plt.plot(pred.detach().numpy())
-plt.plot(data)
-plt.plot(guess)
+vals = [i for i in model(inp.double())]
+plt.plot(data.detach().cpu().numpy())
+plt.plot(vals)
+plt.plot(guess.detach().cpu().numpy())
 plt.show()
